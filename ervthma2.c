54005c0e6e485a75e068e0de9aa276a2827de0c2
Lazaros Topalis <lazarostop32@gmail.com>
2022-04-22 22:00:49 +0300

    change type of array

--- ervthma2.c
+++ ervthma2.c
@@ -14,9 +14,9 @@ typedef struct tag_ocean
   float O2ml_L;
 } ocean;
 
-void printthisshit(ocean array[],int n);
+void printthisshit(ocean array[], int n);
 void printValues(ocean values[]);
-void Heapify(float array[],int node,int heap_size);
+void Heapify(ocean array[], int node, int heap_size);
 void swap(float *x, float *y);
 void HeapSort(float array[], int heap_size);
 
@@ -42,19 +42,19 @@ int main()
   }
 */
 
- rewind(fp);
+  rewind(fp);
   i = 0;
   while (fgets(buff, 1406, fp))
   {
     field_count = 0;
     row_count++;
 
-    //char *field = strtok(buff, ","); // xwrizei
-    char *field = (char *)malloc(sizeof(char)*500);
+    // char *field = strtok(buff, ","); // xwrizei
+    char *field = (char *)malloc(sizeof(char) * 500);
     strcpy(field, strtok(buff, ","));
     if (row_count == 0)
       continue; // oxi
-    while (field_count<8)
+    while (field_count < 8)
     {
       if (field_count == 0)
         strcpy(values[i].date, field);
@@ -74,25 +74,21 @@ int main()
         values[i].O2ml_L = atof(field);
       field = strtok(NULL, ","); // updates values
       field_count++;
-
     }
 
     i++;
     free(field);
   }
 
-  //printValues(values);
-  int k= sizeof(values) / sizeof(values[0]);
+  // printValues(values);
+  int k = sizeof(values) / sizeof(values[0]);
 
-  HeapSort(values,k);
-  printthisshit(values,k);
+  HeapSort(values, k);
+  printthisshit(values, k);
   fclose(fp);
-
-
 
   return 0;
 }
-
 
 void printValues(ocean values[])
 {
@@ -101,64 +97,62 @@ void printValues(ocean values[])
 
   for (i = 1; i < 1406; i++)
   {
-    printf("%s\t %.2f\t %.2f\t %.2f\t %.2f\t %.2f\t %.2f\t %.2f\t ", values[i].date, values[i].T_degC,  values[i].PO4uM, values[i].SiO3uM, values[i].NO2uM, values[i].NO3uM, values[i].Salnty, values[i].O2ml_L);
-  printf("\n");}
-
+    printf("%s\t %.2f\t %.2f\t %.2f\t %.2f\t %.2f\t %.2f\t %.2f\t ", values[i].date, values[i].T_degC, values[i].PO4uM, values[i].SiO3uM, values[i].NO2uM, values[i].NO3uM, values[i].Salnty, values[i].O2ml_L);
+    printf("\n");
+  }
 }
-void Heapify(float array[],int node,int heap_size){
-    int bigger=node;//root
-    int left = 2 *node + 1; // node is an index in array[]
-    int right = 2 *node + 2;
-    // If left child is bigger than root
-    if (left < heap_size&& array[left] > array[bigger]) bigger= left;
+void Heapify(ocean array[], int node, int heap_size)
+{
 
-    // If right child is bigger than (int) bigger
-    if (right < heap_size && array[right] > array[bigger])
-       bigger= right;
+  int bigger = node;       // root
+  int left = 2 * node + 1; // node is an index in array[]
+  int right = 2 * node + 2;
+  // If left child is bigger than root
+  if (left < heap_size && array[left] > array[bigger])
+    bigger = left;
 
-    // If bigger is not root
-    if (bigger != node) {
-        swap(&array[node], &array[bigger]);
+  // If right child is bigger than (int) bigger
+  if (right < heap_size && array[right] > array[bigger])
+    bigger = right;
+
+  // If bigger is not root
+  if (bigger != node)
+  {
+    swap(&array[node], &array[bigger]);
 
     //  heapify the sub-tree
-        Heapify(array, bigger, heap_size);
-    }
+    Heapify(array, bigger, heap_size);
+  }
 }
 
- void swap(float *x, float *y){
-  float temp=*x;
-  *x=*y;
-  *y=temp;
-
+void swap(float *x, float *y)
+{
+  float temp = *x;
+  *x = *y;
+  *y = temp;
 }
-void HeapSort(float array[], int heap_size){
-    // Build heap
- for (int i = heap_size / 2 - 1; i >= 0; i--) Heapify(array, i, heap_size);
- // One by one extract an element from heap
-    for (int i = heap_size - 1; i > 0; i--) {
-        // Move current root to end
-        swap(&array[0], &array[i]);
-
-        // call max heapify on the reduced heap
-        Heapify(array, 0, i);
-    }
-}
-void printthisshit(ocean array[],int n){
-for (int i = 0; i < n; i++)
+void HeapSort(float array[], int heap_size)
+{
+  // Build heap
+  for (int i = heap_size / 2 - 1; i >= 0; i--)
+    Heapify(array, i, heap_size);
+  // One by one extract an element from heap
+  for (int i = heap_size - 1; i > 0; i--)
   {
-    printf("%f\t",array[i].PO4uM);
+    // Move current root to end
+    swap(&array[0], &array[i]);
+
+    // call max heapify on the reduced heap
+    Heapify(array, 0, i);
+  }
+}
+void printthisshit(ocean array[], int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    printf("%f\t", array[i].PO4uM);
     printf("\n");
-
-
-
+  }
 }
-}
-
-
-
-
-
-
-
 
 //}
