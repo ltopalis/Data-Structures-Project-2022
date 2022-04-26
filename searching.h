9ed689a709b8738935edfa5c09d799817807a9f7
Lazaros Topalis <lazarostop32@gmail.com>
2022-04-26 17:08:10 +0300

    creating optimized interpolation search

--- searching.h
+++ searching.h
@@ -3,10 +3,17 @@
 #define SEARCHING_H
 
 /* 
-    interpolation search (O(log log n)) given the array, the searching date and 
+    interpolation search (O(sqrt(n))) given the array, the searching date and 
     the number of elements, the array contains 
 */
 int interpolation_search(table_data *array, time_t date, int n);
+
+/*
+    interpolation search optimized in worst case given the array, the searching 
+    date and the number of elements, the array contains 
+*/
+int optimized_interpolation_search(table_data *array, time_t date, int n);
+
 /*
     linear search (O(n)) given the array, the searching date, the position where we start 
     to search and the final position (included it)
