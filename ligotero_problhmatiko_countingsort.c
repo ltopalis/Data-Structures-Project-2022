bb6fe9778cf6c6ecb4f61a2f94468431d5b7b10b
stellatsimi <83275101+stellatsimi@users.noreply.github.com>
2022-04-29 16:25:57 +0300

    Update and rename problhmatiko_countingsort.c to ligotero_problhmatiko_countingsort.c

--- ligotero_problhmatiko_countingsort.c
+++ ligotero_problhmatiko_countingsort.c
@@ -89,7 +89,7 @@ int main()
 
 void countingsort(ocean array[], int size)
 {
-  ocean final[N];
+  ocean output[size+1];
 
   // Find the largest
   int max = array[0].PO4uM * 100;
@@ -98,7 +98,7 @@ void countingsort(ocean array[], int size)
     if (array[i].PO4uM * 100 > max)
       max = array[i].PO4uM * 100;
   }
-  fprintf(stderr, "max %d\n", max);
+
   int new_array[max + 1];
   // Initialize count array with all zeros.
   for (int i = 0; i <= max + 1; i++)
@@ -115,34 +115,35 @@ void countingsort(ocean array[], int size)
   {
     new_array[i] += new_array[i - 1];
   }
-  fprintf(stderr, "OK\n");
+
 
   // Find the index of each element of the original array in count array, and
   // place the elements in output array
+  //for(int i=0;i<size;i++)
   for (int i = size - 1; i >= 0; i--)
   {
-    strcpy(final[new_array[(int)(array[i].PO4uM * 100)] ].date, array[i].date);
-    final[new_array[(int)(array[i].PO4uM * 100)] ].T_degC = array[i].T_degC;
-    final[new_array[(int)(array[i].PO4uM * 100)] ].PO4uM = array[i].PO4uM;
-    final[new_array[(int)(array[i].PO4uM * 100)] ].NO2uM = array[i].NO2uM;
-    final[new_array[(int)(array[i].PO4uM * 100)] ].NO3uM = array[i].NO3uM;
-    final[new_array[(int)(array[i].PO4uM * 100)] ].O2ml_L = array[i].O2ml_L;
-    final[new_array[(int)(array[i].PO4uM * 100)] ].Salnty = array[i].Salnty;
-    final[new_array[(int)(array[i].PO4uM * 100)] ].SiO3uM = array[i].SiO3uM;
+    strcpy(output[new_array[(int)(array[i].PO4uM * 100)] ].date, array[i].date);
+    output[new_array[(int)(array[i].PO4uM * 100)]-1].T_degC = array[i].T_degC;
+    output[new_array[(int)(array[i].PO4uM * 100)]-1 ].PO4uM = array[i].PO4uM;
+    output[new_array[(int)(array[i].PO4uM * 100)]-1 ].NO2uM = array[i].NO2uM;
+    output[new_array[(int)(array[i].PO4uM * 100)]-1 ].NO3uM = array[i].NO3uM;
+    output[new_array[(int)(array[i].PO4uM * 100)]-1 ].O2ml_L = array[i].O2ml_L;
+    output[new_array[(int)(array[i].PO4uM * 100)]-1 ].Salnty = array[i].Salnty;
+    output[new_array[(int)(array[i].PO4uM * 100)]-1 ].SiO3uM = array[i].SiO3uM;
     new_array[(int)(array[i].PO4uM * 100)]--;
   }
-  fprintf(stderr, "OK2\n");
+
   // Copy the sorted elements into original array
   for (int i = 0; i < size; i++)
   {
-    strcpy(array[i].date, final[i].date);
-    array[i].T_degC = final[i].T_degC;
-    array[i].PO4uM = final[i].PO4uM;
-    array[i].NO2uM = final[i].NO2uM;
-    array[i].NO3uM = final[i].NO3uM;
-    array[i].O2ml_L = final[i].O2ml_L;
-    array[i].Salnty = final[i].Salnty;
-    array[i].SiO3uM = final[i].SiO3uM;
+    strcpy(array[i].date, output[i].date);
+    array[i].T_degC = output[i].T_degC;
+    array[i].PO4uM = output[i].PO4uM;
+    array[i].NO2uM = output[i].NO2uM;
+    array[i].NO3uM = output[i].NO3uM;
+    array[i].O2ml_L = output[i].O2ml_L;
+    array[i].Salnty = output[i].Salnty;
+    array[i].SiO3uM = output[i].SiO3uM;
   }
 }
 void printarray(ocean array[], int n)
