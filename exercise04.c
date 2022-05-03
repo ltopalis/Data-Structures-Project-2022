ad45cb690d58cb9b890159e7af019406b98ff5aa
Lazaros Topalis <lazarostop32@gmail.com>
2022-05-03 16:00:05 +0300

    optimized UI

--- exercise04.c
+++ exercise04.c
@@ -9,11 +9,11 @@
 
 int main()
 {
-    int k;
+    int k, m, choice, flag;
     table_data *ocean = (table_data *)malloc(sizeof(table_data) * DATA);
     char *contents = (char *)malloc(sizeof(char) * LINE_SIZE);
     struct tm t;
-    clock_t c1, c2;
+    clock_t c1, c2, c3, c4;
 
     if (!open_file("ocean.csv", ocean, contents))
     {
@@ -22,7 +22,7 @@ int main()
     }
 
     bubble_sort(ocean, DATA, TIME);
-    //print_array(ocean, DATA);
+    // print_array(ocean, DATA);
 
     do
     {
@@ -40,25 +40,70 @@ int main()
         k = scanf("%d", &t.tm_year);
     } while (!(t.tm_year > 0));
 
-    t.tm_mon  -= 1;
+    t.tm_mon -= 1;
     t.tm_year -= 1900;
-    t.tm_sec   = 0;  /* seconds,  range 0 to 59          */
-    t.tm_min   = 0;  /* minutes, range 0 to 59           */
-    t.tm_hour  = 0; /* hours, range 0 to 23             */
-    t.tm_wday  = 0; /* day of the week, range 0 to 6    */
-    t.tm_yday  = 0; /* day in the year, range 0 to 365  */
+    t.tm_sec = 0;  /* seconds,  range 0 to 59          */
+    t.tm_min = 0;  /* minutes, range 0 to 59           */
+    t.tm_hour = 0; /* hours, range 0 to 23             */
+    t.tm_wday = 0; /* day of the week, range 0 to 6    */
+    t.tm_yday = 0; /* day in the year, range 0 to 365  */
     t.tm_isdst = -1;
 
     c1 = clock();
-    k = optimized_binary_interpolation_search(ocean, mktime(&t), DATA);
+    k = binary_interpolation_search(ocean, mktime(&t), DATA);
     c2 = clock();
 
-    if (k != -1)
-        printf("%d/%d/%d\n", ocean[k].date.tm_mon + 1, ocean[k].date.tm_mday, ocean[k].date.tm_year + 1900);
-    else
-        printf("Record hasn't been found!\n");
+    c3 = clock();
+    m = optimized_binary_interpolation_search(ocean, mktime(&t), DATA);
+    c4 = clock();
 
-    printf("Arxi: %ld, Telos: %ld, Xronos: %f\n", c1, c2, (float)(c2 - c1)/CLOCKS_PER_SEC);
+    printf("====================\n");
+    printf("1. Temperature\n");
+    printf("2. Phosphate\n");
+    printf("3. Both\n");
+    do
+    {
+        printf("Choice? ");
+        scanf("%d", &choice);
+        flag = choice < 1 || choice > 3;
+        if (flag)
+            fprintf(stderr, "ERROR! Should be between 1 and 3!\n");
+
+    } while (flag);
+    printf("====================\n");
+
+    if (k == m)
+    {
+        if (k != -1)
+        {
+            switch (choice)
+            {
+            case 1:
+                printf("%d/%d/%d\tTemperature: %.3f\n", ocean[k].date.tm_mon + 1,
+                       ocean[k].date.tm_mday, ocean[k].date.tm_year + 1900, ocean[k].T_degC);
+                break;
+            case 2:
+                printf("%d/%d/%d\tPhospate: %.3f\n", ocean[k].date.tm_mon + 1,
+                       ocean[k].date.tm_mday, ocean[k].date.tm_year + 1900, ocean[k].PO4uM);
+                break;
+            case 3:
+                printf("%d/%d/%d\tTemperature:%.3f\tPhospahte: %.3f\n", ocean[k].date.tm_mon + 1,
+                       ocean[k].date.tm_mday, ocean[k].date.tm_year + 1900, ocean[k].T_degC, ocean[k].PO4uM);
+                break;
+            default:
+                fprintf(stderr, "An unexpected error occured! Try again, please!\n");
+                break;
+            }
+        }
+        else
+            printf("There're no records!\n");
+
+        printf("====================\n");
+        printf("Binary Interpolation Search           %f sec\n", (float)(c2 - c1) / CLOCKS_PER_SEC);
+        printf("Optimized Binary Interpolation Search %f sec\n", (float)(c4 - c3) / CLOCKS_PER_SEC);
+    }
+    else
+        fprintf(stderr, "An unexpected error occured! Try again, please!\n");
 
     free(ocean);
     free(contents);
