60f4a5da07d07306e75d43749176784654e5c03e
Christina-Eleanna Samara <85516294+christinasamara@users.noreply.github.com>
2022-05-10 12:23:10 +0300

    Update mainforex3.c

--- xristina/mainforex3.c
+++ xristina/mainforex3.c
@@ -4,6 +4,7 @@
 #include "fundamentals.h"
 #include "file_manager.h"
 #include "exe3.h"
+#include "sort_functions.h"
 
 int main()
 {
@@ -11,9 +12,15 @@ int main()
   char contents[LINE_SIZE];
   int year, mon, day;
   struct tm timeinfo;
-  printf("Give year, month and day.\n");
-  scanf("%d\t%d\t%d", &timeinfo.tm_year, &timeinfo.tm_mon, &timeinfo.tm_mday);
+  do
+  {
+    printf("Give year, month and day.\n");
+    scanf("%d\t%d\t%d", &year, &mon, &day);
+  } while (year < 0 || mon < 0 || day <= 0);
+
   open_file("ocean.csv", values, contents);
+
+  bubble_sort(values, DATA, TIME);
 
   timeinfo.tm_year = year - 1900;
   timeinfo.tm_mon = mon - 1;
@@ -22,16 +29,25 @@ int main()
   timeinfo.tm_min = 0;
   timeinfo.tm_sec = 0;
 
-  printf("Give year, month and day.\n");
-  scanf("%d\t%d\t%d", &timeinfo.tm_year, timeinfo.tm_mon, &timeinfo.tm_mday);
+  double time1 = 0.0;
+  clock_t start1 = clock();
+  int n = binarySearch(values, 0, DATA - 1, mktime(&timeinfo));
+  clock_t end1 = clock();
+  time1 = (double)(end1 - start1) / CLOCKS_PER_SEC;
 
-  int n = binarySearch(values, 0, DATA, mktime(&timeinfo));
+  double time2 = 0.0;
+  clock_t start2 = clock();
+  int m = interpolationSearch(values, 0, DATA - 1, mktime(&timeinfo));
+  clock_t end2 = clock();
+  time2 = (double)(end2 - start2) / CLOCKS_PER_SEC;
 
+  // printf("%d\n", n);
   if (n != -1)
   {
     int choice;
     printf("\n1.Temperature\n2.Phosphate\n3.Both\n(1, 2 or 3)\n");
     scanf("%d", &choice);
+    printf("date: %d %d %d\n", values[n].date.tm_year + 1900, values[n].date.tm_mon + 1, values[n].date.tm_mday);
     if (choice == 1)
     {
       printf("%.2f", values[n].T_degC);
@@ -49,5 +65,7 @@ int main()
   {
     printf("date not found.\n");
   }
+  printf("\nBinarySearch: %.10f", time1);
+  printf("\nInterpolationSearch: %.10f", time2);
   return 0;
 }
