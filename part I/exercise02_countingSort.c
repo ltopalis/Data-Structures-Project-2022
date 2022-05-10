79ae175d93ea57b813f6698833ccdd4fc04d2cca
stellatsimi <83275101+stellatsimi@users.noreply.github.com>
2022-05-10 12:22:23 +0300

    Update exercise02_countingSort.c

--- part I/exercise02_countingSort.c
+++ part I/exercise02_countingSort.c
@@ -3,17 +3,21 @@
 #include "fundamentals.h"
 #include "file_manager.h"
 #include "sort_functions.h"
+#include <time.h>
 
 int main()
 {
     table_data values[1405];
     char *contents = (char *)malloc(sizeof(char) * LINE_SIZE);
-
+    clock_t c1, c2;
     open_file("ocean.csv", values, contents);
     int k = sizeof(values) / sizeof(values[0]);
+    c1 = clock();
     countingsort(values, k);
+    c2 = clock();
     printArray(values, k);
-
+    printf("====================\n");
+        printf("Countingsort          %f sec\n", (float)(c2 - c1) / CLOCKS_PER_SEC);
     free(contents);
     exit(0);
 }
