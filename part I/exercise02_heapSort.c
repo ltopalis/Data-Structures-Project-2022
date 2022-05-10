3c2d27186f7698c7d9171f7db6ff89576863c5ab
stellatsimi <83275101+stellatsimi@users.noreply.github.com>
2022-05-10 12:25:57 +0300

    Update exercise02_heapSort.c

--- part I/exercise02_heapSort.c
+++ part I/exercise02_heapSort.c
@@ -3,16 +3,21 @@
 #include "fundamentals.h"
 #include "file_manager.h"
 #include "sort_functions.h"
+#include <time.h>
 
 int main()
 {
     table_data values[1405];
     char *contents = (char *)malloc(sizeof(char) * LINE_SIZE);
-
+    clock_t c3, c4;
     open_file("ocean.csv", values, contents);
     int k = sizeof(values) / sizeof(values[0]);
+    c3 = clock();
     HeapSort(values, k);
+    c4 = clock();
     printArray(values, k);
+    printf("====================\n");
+    printf("Heapsort %f sec\n", (float)(c4 - c3) / CLOCKS_PER_SEC);
 
     free(contents);
 
