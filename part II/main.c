4610ae5cbbff9031c6287727b776890a01b6a795
Lazaros Topalis <lazarostop32@gmail.com>
2022-05-17 22:33:54 +0300

    main

--- part II/main.c
+++ part II/main.c
@@ -13,38 +13,5 @@ int main()
 
     root = insert_from_file("ocean.csv", root);
 
-    while (TRUE)
-    {
-        do
-        {
-
-            printf("1. AVL\n");
-            printf("2. Hashing\n");
-            printf("3. exit\n");
-            printf("choice? ");
-            scanf("%d", &choice);
-            check = (choice > 0 && choice < 4);
-            if (!check)
-            {
-                system("clear");
-                fprintf(stderr, "Should be between 1 and 3\n");
-                print_equal(25);
-            }
-        } while (!check);
-        system("clear");
-
-        switch (choice)
-        {
-        case 1: // AVL
-            break;
-        case 2: // Hashing
-            break;
-        case 3: // exit
-            printf("Bye Bye\n");
-            exit(0);
-        default:
-            fprintf(stderr, "Error\n");
-            break;
-        }
-    }
+    
 }
